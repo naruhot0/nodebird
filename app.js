947cbec8db4b6f0cb51ab2c1c82f2ad140011861
kaka <johndoe@example.com>
2023-11-09 13:16:17 +0900

    2nd

--- app.js
+++ app.js
@@ -5,59 +5,48 @@ const path = require('path');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
-const passport = require('passport')
-const router = require('./routes/page');
 
 dotenv.config();
 const pageRouter = require('./routes/page');
-const {sequelize} = require('./models');
 
 const app = express();
 app.set('port', process.env.PORT || 8001);
 app.set('view engine', 'html');
+nunjucks.configure('views',{
+    express: app,
+    watch: true,
+});
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser(process.env.COOKEI_CECRET));
 app.use(session({
     resave: false,
     saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-      httpOnly: true,
-      secure: false,
+    secret: process.env.COOKEI_CECRET,
+    cookie:{
+        httpOnly: true,
+        secure: false,
     },
-  }));
+}));
 
-  app.use('/', pageRouter);
+app.use('/', pageRouter);
 
-  app.use((req, res, next) => {
-    const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
     error.status = 404;
     next(error);
-  });
+});
 
-  app.use((err, req, res, next) => {
+app.use((err, rep, res, next) => {
     res.locals.message = err.message;
     res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
     res.status(err.status || 500);
     res.render('error');
-  });
+});
 
-  app.listen(app.get('port'), () => {
-    console.log(app.get('port'), '번 포트에서 대기중');
-  });
-
-  nunjucks.configure('views',{
-    express: app,
-    watch: true,
-  });
-  sequelize.sync({force: false}).then(() => {
-    console.log('데이터베이스 연결성공');
-  }).catch((err) => {
-    console.error(err);
-  });
-
-  app.use(morgan('dev'));
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중');
+});
